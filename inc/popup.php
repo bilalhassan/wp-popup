a2c59de1151e11b6b82f0d1a1958005fac866539
bilalhassan <info@ottawapc.ca>
2014-06-16 20:13:04 -0400

    removed hardcoded values

--- inc/popup.php
+++ inc/popup.php
@@ -4,7 +4,26 @@
  * @author bilal hassan <info@smartcatdesign.net>
  * 
  */
+$color = get_option('sc_popup_color');
 ?>
+<style type="text/css">
+    .sc-popup-title{
+        background: <?php echo $color;?>;
+    }
+    .sc-popup-cta a{
+        color: <?php echo $color;?>;
+    }
+    #sc-popup input[type='text']:focus,
+    #sc-popup input[type='email']:focus{
+        border: 1px solid <?php echo $color;?>;
+        box-shadow: 0 0 3px <?php echo $color;?>;
+        -moz-box-shadow: 0 0 3px <?php echo $color;?>;
+        -webkit-box-shadow: 0 0 3px <?php echo $color;?>;
+    }
+    #sc-popup input[type='submit']{
+        background: <?php echo $color;?>;
+    }
+</style>
 <div id="sc-popup-dim">
     <div id="sc-popup">
         <div id="sc-close"><img src="<?php echo plugins_url(); ?>/wp-timed-popup/images/close.png"/></div>
@@ -14,7 +33,7 @@
             </div>
 
             <div class="sc-popup-media left width50">
-                <iframe width="560" height="315" src="//www.youtube.com/embed/666y56PpwoQ" frameborder="0" allowfullscreen></iframe>
+                <?php echo get_option('sc_popup_media_link'); ?>
             </div>
 
             <div class="left width50">
@@ -26,7 +45,7 @@
             <div class="clear"></div>
 
             <div class="sc-popup-form">
-                <?php echo do_shortcode('[mc4wp_form]'); ?>
+                <?php echo do_shortcode(get_option('sc_popup_shortcode')); ?>
             </div>
 
             <div class="sc-popup-cta">
